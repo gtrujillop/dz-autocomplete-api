c3a84625d111f8d0873894e416732f6b0c4ce084
Gaston Trujillo <gaston.trujillo.java@gmail.com>
2016-12-20 20:33:12 -0500

    Routes: Set default path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'suggestions#index'
   resources :suggestions, only: [:index]
   resources :categories, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
